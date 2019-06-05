d7ed892b82cacd6507ffeed101634c1fa30cbc85
cluster <clusternlh@gmail.com>
2019-06-05 23:59:34 +0800

    Update teacher.cpp

--- Two_Sum/teacher.cpp
+++ Two_Sum/teacher.cpp
@@ -9,7 +9,7 @@ public:
             auto k = m1.find(nums[i]);
             if ( k != m1.end())
             {
-                res.push_back(m1[nums[i]]);
+                res.push_back(m1[nums[i]]);     // hint : 这里使用map来查询(4/8ms)，比k->second访问快速很多(16ms)
                 res.push_back(j);
                 break;
             }
