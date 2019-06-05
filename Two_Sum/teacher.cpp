eae40d9666024d95684ffe5b92029dc1b81f3f24
cluster <clusternlh@gmail.com>
2019-06-05 23:56:54 +0800

    Create teacher.cpp

---
+++ Two_Sum/teacher.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> res;
+        std::map<int,int> m1;
+        int j = 0;
+        for(size_t i =0 ;i<nums.size();i++)
+        {
+            auto k = m1.find(nums[i]);
+            if ( k != m1.end())
+            {
+                res.push_back(m1[nums[i]]);
+                res.push_back(j);
+                break;
+            }
+            else{
+                m1[target-nums[i]] = j;
+            }
+            j++;
+        }
+        return res;
+    }
+};
